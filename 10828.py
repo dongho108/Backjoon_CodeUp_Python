e011d85184f50ebc8389bf66407d6d5a07d08de1
K_Boan <54317630+dongho108@users.noreply.github.com>
2020-03-07 15:24:29 +0900

    데이터 가변적일때, class사용시 주의점, stack구현시 주의점
    * 입력받는 데이터 수가 가변적일때 : 입력받는곳을 하나의 변수로 두면 자동으로 배열로 생성된다.
    * 입력받는 데이터 형이 다를 때 : split으로 문자열로 입력받은 후 사용할 때 형변환 한다.
    * class사용시 주의점 : classname.value로 전역변수 사용해도 되지만, 생성자 안에서 self로 만들어도 다른 메소드에서도 사용이 가능하다.
    * stack구현시 주의점 : 
         1. pop할때 뺄 변수를 미리 다른 곳에 담아두고 뺀다음 리턴값으로 준다.
         2. top구현을 하려면 size를 세는 변수를 선언해놔야한다.

--- 10828.py
+++ 10828.py
@@ -1,37 +1,45 @@
-class Stack:
-    stack = []
-    def push(self, input):
-        Stack.stack.append(input)
-    def pop(self):
-        if not stack:
-            print(-1)
-        else:
-            print(stack[-1])
-            Stack.stack.pop()
-    def size(self):
-        print(len(Stack.stack))
-    def empty(self):
-        if not stack:
-            print(1)
-        else:
-            print(0)
-    def top(self):
-        if not stack:
-            print(-1)
-        else:
-            print(stack[0])
+import sys
+input = sys.stdin.readline
 
-num = 14
+class Stack:
+    def __init__(self):
+        self.stack = []
+        self._size = 0
+    def push(self, input):
+        self.stack.append(input)
+        self._size = self._size + 1
+    def pop(self):
+        if not self.stack:
+            return -1
+        else:
+            self.temp = self.stack[-1]
+            self.stack.pop()
+            self._size = self._size - 1
+            return self.temp
+    def size(self):
+        return self._size
+    def empty(self):
+        if not self.stack:
+            return 1
+        else:
+            return 0
+    def top(self):
+        if not self.stack:
+            return -1
+        else:
+            return self.stack[self._size-1]
+
+num = int(input())
 st = Stack()
 for i in range(0,num):
-    in_str, in_num = input().split()
-    if in_str == "push":
-        st.push(int(in_num))
-    elif in_str == "pop":
-        st.pop()
-    elif in_str == "size":
-        st.size()
-    elif in_str == "empty":
-        st.empty()
-    elif in_str == "top":
-        st.top()
+    in_put = input().strip().split()
+    if in_put[0] == "push":
+        st.push(int(in_put[1]))
+    elif in_put[0] == "pop":
+        print(st.pop())
+    elif in_put[0] == "size":
+        print(st.size())
+    elif in_put[0] == "empty":
+        print(st.empty())
+    elif in_put[0] == "top":
+        print(st.top())
